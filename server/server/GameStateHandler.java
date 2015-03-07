aaac78cdbdae12fca74f25d0a79cfc62843d1a36
kanaia <smb.sb@web.de>
2015-03-07 03:42:53 +0100

    Mit Vorsicht zu genießen, spielablauf in grundzügen fertig.

--- server/server/GameStateHandler.java
+++ server/server/GameStateHandler.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 import java.util.Vector;
 
 import ui.ServerUI;
+import util.WordDatabase;
 
 
 public class GameStateHandler 
@@ -10,6 +11,11 @@ public class GameStateHandler
 	private ServerUI ui;
 	private Vector <ConnectionHandler> IDList;
 	private boolean isActive = false;
+	private WordDatabase w = new WordDatabase();
+	private boolean gameActive = false;
+	private String s = "";
+	private int anzahlRichtig = 0;
+	
 	public static void main(String[] args)
 	{
 		GameStateHandler gg = new GameStateHandler();
@@ -29,81 +35,76 @@ public class GameStateHandler
 		}
 	}
 	
-	public void handlePoint(ConnectionHandler c, int x, int y, boolean pulling)
-	{
-		for(int i = 0; i < IDList.size(); i++)
-		{
-			if(IDList.elementAt(i) != c)
-			{
-				if(pulling)
-				{
-					IDList.elementAt(i).p.writeMessage("draw" + "1" + x + "," + y);
-				}
-				else
-				{
-					IDList.elementAt(i).p.writeMessage("draw" + "0" + x + "," + y);
-				}
+
+	public void startGame(){
+		//ID des Spielers am Zug
+		int amZug = 0;
+		//Anzahl Runden
+		int numberOfRounds = IDList.size() * 2;
+		int gameCounter = 0;
+		String zuZeichnen = "";
+		gameActive = true;
+		while(gameActive){
+			s = w.getRandomWord();
+			IDList.elementAt(amZug).myTurn = true;
+			zuZeichnen = "Du bist am Zug. Dein Wort ist " + s + ".";
+			sendPrivateMessage(IDList.elementAt(amZug), zuZeichnen);
+			while(!(anzahlRichtig >= IDList.size())){
+				//ja, was tut man hier?
+				//hints geben?
+				//verdammt, ich brauch n timer. irgendwas was seperat läuft.
+				//der Text sollte ja gehandlet werden. also parallel. in handleText wird auch anzahlRichtig erhöht.
 			}
+			//nächster Spieler ist dran
+			if(amZug == IDList.size()){
+				amZug = 0;
+			}else{
+				amZug++;
+			}
+			//Spielende
+			gameCounter++;
+			if(gameCounter > numberOfRounds){
+				gameActive = false;
+			}
+			//Gott, ich bin müde.
 		}
 	}
 	
+	private void sendPrivateMessage(ConnectionHandler c, String msg) {
+		c.p.writeMessage("text" + msg);
+	}
+	
+	private void sendWinMessage(ConnectionHandler c){
+		if(anzahlRichtig == 0){
+			c.p.writeMessage("text" + "Glückwunsch! Du hast es als erstes erraten! (3 Punkte)")
+			c.points = c.points + 3;
+		}else if(anzahlRichtig >= 1){
+			c.p.writeMessage("text" + "Das Wort wurde bereits erraten, du belegst den " + anzahlRichtig+1 + ". Platz. (1 Punkt)");
+			c.points = c.points + 1;
+		}
+	}
+
 	public void handleText(ConnectionHandler c, String msg)
 	{
-		for(int i = 0; i < IDList.size(); i++)
-		{
-			IDList.elementAt(i).p.writeMessage("text" + c.name + ": " + msg);
-		}
-	}
-	
-	public void submitScore(String scoreBoard)
-	{
-		for(int i = 0; i < IDList.size(); i++)
-		{
-			IDList.elementAt(i).p.writeMessage("list" + scoreBoard);
-		}
-	}
-	
-	public void handleColorChange(ConnectionHandler c, int rgb)
-	{
-		for(int i = 0; i < IDList.size(); i++)
-		{
-			if(IDList.elementAt(i) != c)
+		if(gameActive && IDList.elementAt(i) == c && w.isGuessCorrect(msg, s)){
+			sendWinMessage(c);
+			anzahlRichtig++;
+		}else{
+			for(int i = 0; i < IDList.size(); i++)
 			{
-				IDList.elementAt(i).p.writeMessage("colr" + rgb);
+				if(IDList.elementAt(i) != c){
+					IDList.elementAt(i).p.writeMessage("text" + IDList.elementAt(i).name + ";" + msg);
+				}
 			}
 		}
 	}
 	
-	public void handleSizeChange(ConnectionHandler c, int size)
+	public void handleNameChange(ConnectionHandler c, String newName, String oldName)
 	{
 		for(int i = 0; i < IDList.size(); i++)
 		{
-			if(IDList.elementAt(i) != c)
-			{
-				IDList.elementAt(i).p.writeMessage("size" + size);
-			}
+			IDList.elementAt(i).p.writeMessage("name" + newName + ";" + oldName);
 		}
-	}
-	
-	public void handleWord(ConnectionHandler c, String word)
-	{
-		// WORD HAS BEEN CHOSEN
-	}
-	
-	public void handleConnect(ConnectionHandler c, String newName)
-	{
-		for(int i = 0; i < IDList.size(); i++)
-		{
-			if(IDList.elementAt(i) != c)
-			{
-				IDList.elementAt(i).p.writeMessage("newc" + newName);
-			}
-		}
-	}
-	
-	public void sendWinMessage(ConnectionHandler c, String msg)
-	{
-		//
 	}
 	
 	public void addToList(ConnectionHandler c)
@@ -117,6 +118,5 @@ public class GameStateHandler
 	
 	public void newPlayerConnected(String name, String ip) {
 		ui.newPlayerConnected(name, ip);
-		
 	}
 }
